3dbd0a357f9d7c0cd50c8461c7dd7c1b109eb005
Nic Mitchell <nic@nicmitchell.com>
2015-05-07 15:01:22 -0700

    added pseudocode

--- render.js
+++ render.js
@@ -1,4 +1,29 @@
 // load json data via AJAX
 // parse json data
+  // create big ass object
+  // recurse through children
+
+// base case: if no children
+  // return
+// create opening tag
+  // add "<" + tag 
+// if has attributes
+  // add all attributes
+    // for each attribute
+      // add attribute key to tag
+      // add attribute value in double quotes
+      // if attribute key is "class"
+        // add each array item to class
+// close the tag, add ">"
+// write current value to the DOM
+// if has content, write it to the DOM     
+// if it has children
+  // recurse, passing in children array
+// if it is a self closing tag
+  // add " />"
+// If it is not a self closing tag
+  // add "</ + tag >"
+
 
 // append to body
+
